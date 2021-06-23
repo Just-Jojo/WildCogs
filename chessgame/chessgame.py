d0a8f0df7a3c5810c9eb67ebdef5d0ae4bedefb2
Just-Jojo <justjojogh@gmail.com>
2021-06-22 23:54:11 -0400

    [ChessGame] Turns out, Jojo's an idiot

--- chessgame/chessgame.py
+++ chessgame/chessgame.py
@@ -274,9 +274,7 @@ class ChessGame(commands.Cog):
         )
 
         try:
-            games = await self._config.channel(ctx.channel).games()
-            if not isinstance(games, dict):
-                return await ctx.send("I'm sorry, something went wrong")
+            games = await self._get_games(ctx.channel)
             game = games[game_name]
         except KeyError:
             embed.add_field(
